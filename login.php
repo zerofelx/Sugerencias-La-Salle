418653f51465a02081c9335a3502ba2eef71acf6
Jesus Urquijo <zeroflex3@gmail.com>
2020-10-15 16:22:20 -0500

    Iniciando Login y Register

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php 
+    include ("./database/db.php");
+    include ("./includes/header.php");
+
+    switch ($_GET['form']) {
+        case 'login':
+            $View = 'Login';
+            break;
+        case 'register':
+            $View = 'Register';
+            break;
+        default:
+            $View = 'Login';
+            break;
+    }
+?>
+
+<div class="container p-4">
+    <div class="row justify-content-center">
+        <?php 
+            switch ($View) {
+                case 'Login':
+                    include ("./includes/components/login_form.php");
+                    break;
+                
+                case 'Register':
+                    include ("./includes/components/create_user.php");
+                    break;
+            }
+        ?>
+    </div>
+</div>
+
+<?php
+    include ("./includes/footer.php");
+?>
