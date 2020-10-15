59cc900de0d5c33ab2b276d194d93aca9770aaae
Jesus Urquijo <zeroflex3@gmail.com>
2020-10-15 16:22:46 -0500

    Dando toques finales

--- mlist.php
+++ mlist.php
@@ -15,15 +15,28 @@
                 </tr>
             </thead>
             <tbody>
-                <?php 
-                    $query = "SELECT * FROM modulos";
+                <?php
+                    if (isset($_GET['ciclo'])) {
+                        $ciclo = $_GET['ciclo'];
+                        switch ($ciclo) {
+                            case 'all':
+                                $query = "SELECT * FROM modulos";
+                                break;
+                            
+                            default:
+                                $query = "SELECT * FROM modulos WHERE ciclo='$ciclo'";
+                                break;
+                        }
+                    } else {
+                        $query = "SELECT * FROM modulos";
+                    }
                     $modules = mysqli_query($conn, $query);
 
                     while ($row = mysqli_fetch_array($modules)) {
                         ?>
                         <tr>
                             <td><?php echo $row['ID']; ?></td>
-                            <td><?php echo $row['Modulo']; ?></td>
+                            <td><a href="mdetails.php?module_id=<?=$row['ID'];?>"><?php echo $row['Modulo']; ?></a></td>
                             <td><?php echo $row['Ciclo']; ?></td>
                             <td><?php echo $row['Descripcion']; ?></td>
                         </tr>
