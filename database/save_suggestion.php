599e15ba3dd5fdab86ee2ba64c0372b40fc69df2
Jesus Urquijo <zeroflex3@gmail.com>
2020-10-15 16:21:59 -0500

    Terminando funcionalidad de guardar sugerencias

--- database/save_suggestion.php
+++ database/save_suggestion.php
@@ -1,11 +1,25 @@
 <?php 
 include ("db.php");
-
     if(isset($_POST['save_suggestion'])) {
+        $userid = 1;
+        $module = $_GET['modulo'];
         $title = $_POST['suggestion_title'];
         $suggestion = $_POST['suggestion'];
 
-        $query = "INSERT INTO usuarios(username, psw, rol, locked) VALUES (Andrea, )";
+        $query = "INSERT INTO suggestions(userid, moduloid, titulo, descripcion) VALUES ('$userid', $module, '$title', '$suggestion')";
+        $result = mysqli_query($conn, $query);
+
+        if (!$result) {
+            echo $module;
+            die("Query Failed");
+        }
+
+        $_SESSION['message'] = 'Sugerencia "' . $title . '" creada';
+        $_SESSION['message_type'] = 'success';
+
+        header("Location: http://localhost/mdetails.php?module_id=$module");
     };
 
 ?>
+
+<?php echo $row['Modulo'] . "a";?>
