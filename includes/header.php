ccf420fe539f7bacdf0188e050d93af7e9fc15ed
Jesus Urquijo <zeroflex3@gmail.com>
2020-10-15 16:24:27 -0500

    Agregando fontawesome

--- includes/header.php
+++ includes/header.php
@@ -7,11 +7,14 @@
     <title>Sugerencias La Salle Online</title>
     <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="src/Bootstrap/css/bootstrap.min.css">
+    <!-- FONT AWESOME -->
+    <script src="https://kit.fontawesome.com/a3c7d26cee.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
-<nav class="navbar navbar-dark bg-dark">
+<nav class="navbar navbar-dark bg-dark sticky-top">
     <div class="container">
         <a href="index.php" class="navbar-brand">Sugerencias La Salle Online</a>
+        <a href="login.php" class="navbar-brand">Login</a>
     </div>
 </nav>
