9b035a9a173978b52e0056dc8a2e7e8785e0ba8e
Jesus Urquijo <zeroflex3@gmail.com>
2020-10-16 02:47:23 -0500

    Agregando y terminando funcionalidad de comentarios

---
+++ database/send_comment.php
@@ -0,0 +1,30 @@
+<?php 
+include ("db.php");
+$moduleid = $_GET['module'];
+
+if(isset($_SESSION['user_id'])) {
+    if(isset($_POST['send_comment'])) {
+        if(!empty($_POST['comment'])) {
+            $comment = $_POST['comment'];
+            $suggestionid = $_GET['suggestion'];
+            $userid = $_SESSION['user_id'];
+    
+            $query = "INSERT INTO comments (userid, suggestionid, descripcion) VALUES ('$userid', '$suggestionid', '$comment')";
+            $result = mysqli_query($conn, $query);
+    
+            if(!$result) {
+                die("Query Failed");
+            };
+    
+            $_SESSION['message'] = 'Comentario agregado';
+            $_SESSION['message_type'] = 'success';
+            header("Location: http://localhost/mdetails.php?module_id=$moduleid");
+        } else {
+            $_SESSION['message'] = 'Comentario sin contenido';
+            $_SESSION['message_type'] = 'danger';
+            header("Location: http://localhost/mdetails.php?module_id=$moduleid");
+        }
+    }
+}
+
+?>
