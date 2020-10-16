f8e87d6409da28a4bb4c06958aed4ecde0d79164
Jesus Urquijo <zeroflex3@gmail.com>
2020-10-16 02:46:54 -0500

    Terminando funcionalidad de guardado de sugerencias

--- database/save_suggestion.php
+++ database/save_suggestion.php
@@ -1,25 +1,33 @@
 <?php 
 include ("db.php");
-    if(isset($_POST['save_suggestion'])) {
-        $userid = 1;
-        $module = $_GET['modulo'];
-        $title = $_POST['suggestion_title'];
-        $suggestion = $_POST['suggestion'];
+$module = $_GET['modulo'];
 
-        $query = "INSERT INTO suggestions(userid, moduloid, titulo, descripcion) VALUES ('$userid', $module, '$title', '$suggestion')";
-        $result = mysqli_query($conn, $query);
-
-        if (!$result) {
-            echo $module;
-            die("Query Failed");
-        }
-
-        $_SESSION['message'] = 'Sugerencia "' . $title . '" creada';
-        $_SESSION['message_type'] = 'success';
-
+if(isset($_SESSION['user_id'])) {
+    if(!empty($_POST['suggestion_title']) && !empty($_POST['suggestion'])) {
+        if(isset($_POST['save_suggestion'])) {
+            $userid = $_SESSION['user_id'];
+            $title = $_POST['suggestion_title'];
+            $suggestion = $_POST['suggestion'];
+    
+            $query = "INSERT INTO suggestions(userid, moduloid, titulo, descripcion) VALUES ('$userid', $module, '$title', '$suggestion')";
+            $result = mysqli_query($conn, $query);
+    
+            if (!$result) {
+                echo $userid;
+                die("Query Failed");
+            }
+    
+            $_SESSION['message'] = 'Sugerencia "' . $title . '" creada';
+            $_SESSION['message_type'] = 'success';
+    
+            header("Location: http://localhost/mdetails.php?module_id=$module");
+        };
+    } else {
+        $_SESSION['message'] = 'Sugerencia sin titulo o contenido';
+        $_SESSION['message_type'] = "danger";
         header("Location: http://localhost/mdetails.php?module_id=$module");
-    };
-
+    }
+} else {
+    header("Location: http://localhost/login.php");
+}
 ?>
-
-<?php echo $row['Modulo'] . "a";?>
