627603796f1124933155a054b0ef2251276ba0ee
Jesus Urquijo <zeroflex3@gmail.com>
2020-10-16 02:52:32 -0500

    Agregando elementos básicos de la funcionalidad de usuarios

--- includes/header.php
+++ includes/header.php
@@ -15,6 +15,29 @@
 <nav class="navbar navbar-dark bg-dark sticky-top">
     <div class="container">
         <a href="index.php" class="navbar-brand">Sugerencias La Salle Online</a>
-        <a href="login.php" class="navbar-brand">Login</a>
+        <?php 
+        if(isset($_SESSION['user_id'])) {
+            ?>
+                <a href="profile.php" class="navbar-brand"><?=$_SESSION['username']?></a>
+            <?php
+        } else {
+            ?>
+                <a href="login.php" class="navbar-brand">Login</a>
+            <?php
+        }
+        ?>
+        
     </div>
 </nav>
+
+<?php
+    if (isset($_GET['session'])) {
+        $session = $_GET['session'];
+        if ($session = 'close') {
+            echo $session;
+            session_destroy();
+
+            header("Location: http://localhost/index.php");
+        }
+    }
+?>
